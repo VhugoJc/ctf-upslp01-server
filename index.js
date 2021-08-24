a1535c3f45da82615fb4f574272e58258ee91180
VhugoJc <69024201+VhugoJc@users.noreply.github.com>
2021-08-24 01:11:33 -0500

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const polimemes = require('./polimemes');
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 4000;
 
 polimemes.listen(port, ()=>{
     console.log("App is running on port " + port);
-    console.log("####BACKEND###");
 })
