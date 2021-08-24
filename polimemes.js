85f0bb29678b10ce9e76416f300e286d62200930
VhugoJc <69024201+VhugoJc@users.noreply.github.com>
2021-08-24 00:43:24 -0500

    Update polimemes.js

--- polimemes.js
+++ polimemes.js
@@ -4,9 +4,11 @@ const path = require('path');
 const bodyparser = require('body-parser');
 const app = express();
 const api = express.Router();
+
+//Config:
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
-// Configure Header HTTP para que no dé error de cors
+// Configure Header HTTP para que no de error de cors
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
@@ -20,7 +22,8 @@ app.use((req, res, next) => {
 
 
 
- const addNewStudent = (req, res)=>{//función de validación
+  
+ const addNewStudent = (req, res)=>{//funcion de validacion de credenciales
     let {id,password} = req.body;
     let response ={
       id: id
@@ -42,12 +45,17 @@ app.use((req, res, next) => {
     }
 }
 
+
+//mandar archivo /source
 const downloadFile = (req, res) =>{
   res.download(path.join(__dirname, '/polimemes.js'));
 }
 
+
 const polimemes = api.post('/add-new-student',addNewStudent);
 const getSource = api.get('/source',downloadFile);
+
+
 
 //routes
   app.use('/api',polimemes);
