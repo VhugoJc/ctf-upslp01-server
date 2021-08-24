3e61d2effd5b1a89069970566dd851e0ba21aac7
VhugoJc <69024201+VhugoJc@users.noreply.github.com>
2021-08-24 14:07:39 -0500

    v7

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", "true");
     res.header('Access-Control-Allow-Headers', 'accept', 'Content-Type')
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 });
 
