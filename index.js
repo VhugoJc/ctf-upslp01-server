c0e7eb802c8aaee457057909e91c546904a06233
VhugoJc <69024201+VhugoJc@users.noreply.github.com>
2021-08-24 13:45:46 -0500

    v3

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyparser.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    "Access-Control-Allow-Headers",
+    "Access-Control-Request-Headers",
     "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
